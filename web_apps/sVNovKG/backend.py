e2cabd43a1c50ecf51a6e804f3a1f2f588b3c632
admin <matt@dataiku.com>
2020-03-31 16:23:58 +0800

    Saved web app sparkline (ICAR.sVNovKG)

--- web_apps/sVNovKG/backend.py
+++ web_apps/sVNovKG/backend.py
@@ -4,14 +4,22 @@ from flask import request
 
 
 dataset_name = "SCB_prep"
+df = dataiku.Dataset(dataset_name).get_dataframe()
+col_sparkline = [i for i in df.columns if i.startswith("t_m")]
+col_choice = ["6_month_avg","3_month_avg","previous_fy"]
+col_pred = "model_prediction"
+col_split = "Aggregation"
+col_cat = "Account"
 
 
 @app.route('/init')
 def init():
 
-    mydataset = dataiku.Dataset(dataset_name)
-    mydataset_df = mydataset.get_dataframe(sampling='head', limit=max_rows)
+    df = dataiku.Dataset(dataset_name).get_dataframe()
+    
 
     # Pandas dataFrames are not directly JSON serializable, use to_json()
     data = mydataset_df.to_json()
-    return json.dumps({"status": "ok", "data": data})
+    
+    category = list(df[col_cat].values)
+    return json.dumps({"status": "ok", "data": data, "category":category})
