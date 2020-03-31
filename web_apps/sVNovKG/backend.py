5b95e34643704a44220a52d501613c8028f5b9cb
admin <matt@dataiku.com>
2020-03-31 15:49:39 +0800

    Saved web app sparkline (ICAR.sVNovKG)

--- web_apps/sVNovKG/backend.py
+++ web_apps/sVNovKG/backend.py
@@ -3,18 +3,13 @@ import pandas as pd
 from flask import request
 
 
-# Example:
-# As the Python webapp backend is a Flask app, refer to the Flask
-# documentation for more information about how to adapt this
-# example to your needs.
-# From JavaScript, you can access the defined endpoints using
-# getWebAppBackendUrl('first_api_call')
+dataset_name = "SCB_prep"
 
-@app.route('/first_api_call')
-def first_call():
-    max_rows = request.args.get('max_rows') if 'max_rows' in request.args else 500
 
-    mydataset = dataiku.Dataset("REPLACE_WITH_YOUR_DATASET_NAME")
+@app.route('/init')
+def init():
+
+    mydataset = dataiku.Dataset(dataset_name)
     mydataset_df = mydataset.get_dataframe(sampling='head', limit=max_rows)
 
     # Pandas dataFrames are not directly JSON serializable, use to_json()
