f24a36913972e575d08897200517e1249c11b56d
admin <matt@dataiku.com>
2020-03-31 15:48:15 +0800

    Saved web app sparkline (ICAR.sVNovKG)

--- web_apps/sVNovKG/app.js
+++ web_apps/sVNovKG/app.js
@@ -159,4 +159,8 @@ $( document ).ready(function() {
     sparkline('#sparkline-a-avgRating', generateExecSparklineData(1,4));
 
 
+});$.getJSON(getWebAppBackendUrl('/first_api_call'), function(data) {
+    console.log('Received data from backend', data)
+    const output = $('<pre />').text('Backend reply: ' + JSON.stringify(data));
+    $('body').append(output)
 });
