99eca3b075bd2d73b0458e3c11eaaaa252753b73
admin <matt@dataiku.com>
2020-03-31 22:21:09 +0800

    Saved web app sparkline (ICAR.sVNovKG)

--- web_apps/sVNovKG/backend.py
+++ web_apps/sVNovKG/backend.py
@@ -14,7 +14,14 @@ col_cat = "Account"
 def get_sparkline(df):
     s = list(df[col_sparkline].values)[::-1]
     s.append(df[col_pred])
-    return s
+    d = [str(i) for i in pd.date_range(start='1/1/2018', periods=len(col_sparkline)+1).values]
+    out = zip(d,s)
+    r = []
+    for d0,v0 in out:
+        r.append({"date":d0, "value":v0})
+    return r
+
+df["spark"] = df.apply(get_sparkline, axis=1)
 
 df["spark"] = df.apply(get_sparkline, axis=1)
 for i in col_sparkline:
